f1bd518985b2e8134358ab0c6d501a566efe5e8a
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-19 01:41:40 -0300

    creacion del footer

---
+++ src/component/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+
+function Footer() {
+    return (
+            <footer className='Footer-Aspect'>
+                <div className='Fotter-LogoyCopy'>
+                    
+                    {/* <img src="https://i.imgur.com/YdoATRu.png" alt="logo" className='Fotter-Logo'></img> */}
+                    <p className='mb-0'>Copyright ©Mytinerary|2022</p>
+                </div>
+                <div className='Footer-Parrafo'>
+                    <p><strong>Thanks for visiting!</strong></p>
+                </div>
+
+                
+                <div className='Footer-Link'>
+                    <a className='Footer-Anchor' href="Home">Home</a>
+                    <a className='Footer-Anchor' href="Cities">Cities</a>
+                    </div>
+            </footer>
+
+
+)
+}
+export default Footer;
