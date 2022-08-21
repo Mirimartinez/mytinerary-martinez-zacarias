f527e2bca0672d4f059ee4f5432c1116941c53f9
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-21 16:31:53 -0300

    agrega Hero a components

---
+++ src/component/Hero.js
@@ -0,0 +1,19 @@
+import '../styles/Hero.css';
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <video id='video' autoPlay loop muted>
+      <source
+          src="https://player.vimeo.com/external/534082644.hd.mp4?s=28e9778e97e5a36b94e3e40a4733ce337757788e&profile_id=174&oauth2_token_id=57447761"
+          type="video/mp4"
+        />
+      </video>
+      <h1 className='Hero-title'>MyTinerary</h1>
+      <p className='Hero-subtitle'>Find your perfect trip, designed by insiders who know and love their cities!</p>
+      <button className='Hero-button' type="submit">Let's go!</button>
+    </div>
+  );
+}
+
+export default Hero;
