a7583ff6585d1dc67946b210c0ed9eccac9e0c87
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-21 16:17:18 -0300

    carrusel en pagina de prueba

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import Footer from './component/Footer.js';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import EventsCarousel from './component/EventsCarousel.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <EventsCarousel />
     <Footer />
   </React.StrictMode>
 );
