dfcefdecbd56d7a27ef0dba5e1d92f635b5a4c68
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-21 16:31:19 -0300

    importa hero

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Carousel from './component/Carousel.js';
 import Footer from './component/Footer.js';
 import './index.css';
-import App from './pages/App';
+import Hero from './component/Hero';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hero />
     <Footer />
   </React.StrictMode>
 );
