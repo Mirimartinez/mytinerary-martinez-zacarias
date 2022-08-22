893b9a8844a4924e3a0c2a6bec6814de45017452
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-22 00:37:40 -0300

    actualiza background

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -5,11 +5,11 @@ function UnderConstruction() {
     <div className="UnderConstruction">
       <video id='video' autoPlay loop muted>
       <source
-          src="https://player.vimeo.com/external/534082644.hd.mp4?s=28e9778e97e5a36b94e3e40a4733ce337757788e&profile_id=174&oauth2_token_id=57447761"
+          src="http://localhost:3000/videoHero.mp4"
           type="video/mp4"
         />
       </video>
-      <img className='UnderConstruction-Imagen' src='https://365psd.com/images/previews/107/under-construction-vector-illustration-eps-58368.jpg' ></img>
+      <img className='UnderConstruction-Imagen' src='https://365psd.com/images/previews/107/under-construction-vector-illustration-eps-58368.jpg' alt='' ></img>
       <h1 className='UnderConstruction-title'>Page under construction..</h1>
       <p className='UnderConstruction-subtitle'>We will be available shortly.</p>
       
