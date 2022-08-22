0c29f7e5768766e766439211a188860b14f3c6b8
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-22 00:15:17 -0300

    cambio a under constructios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Footer from './component/Footer.js';
+/* import Footer from './component/Footer.js'; */
 import './index.css';
-import App from './pages/App';
+/* import App from './pages/App'; */
 import reportWebVitals from './reportWebVitals';
-import EventsCarousel from './component/EventsCarousel.js';
+/* import EventsCarousel from './component/EventsCarousel.js'; */
+import UnderConstruction from './pages/UnderConstruction.js';
+import FooterLayout from './layouts/FooterLayout.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UnderConstruction />
+    <FooterLayout />
+    {/* <App />
     <EventsCarousel />
-    <Footer />
+    <Footer /> */}
   </React.StrictMode>
 );
 
