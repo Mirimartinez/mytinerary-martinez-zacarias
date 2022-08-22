8ff4d76f69f9d6de8f63cca3472d99dd2c60cdaf
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-22 17:01:05 -0300

    comenta UnderConstruction

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './component/Carousel.js';
-import Footer from './component/Footer.js';
+// import Footer from './component/Footer.js';
 import './index.css';
 import Hero from './component/Hero';
 import reportWebVitals from './reportWebVitals';
-/* import EventsCarousel from './component/EventsCarousel.js'; */
-import UnderConstruction from './pages/UnderConstruction.js';
+import EventsCarousel from './component/EventsCarousel.js'; 
+// import UnderConstruction from './pages/UnderConstruction.js';
 import FooterLayout from './layouts/FooterLayout.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UnderConstruction />
-    <FooterLayout />
     {/* <App />
+    <UnderConstruction />
+    <Footer /> */}
     <Hero />
     <EventsCarousel />
-    <Footer /> */}
+    <FooterLayout />
   </React.StrictMode>
 );
 
