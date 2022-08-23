ce00568dde20ba473787b6788cfb34757884e634
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-22 22:46:28 -0300

    carrusel manual listo

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -1,33 +1,76 @@
+/* import { clear } from 'console'
+import { func } from 'prop-types' */
+/* import { clearInterval } from 'timers' */
 import '../styles/Carousel.css'
+import Arrow from './Carousel/Arrow'
+import {useState} from 'react'
 
 function Carousel(props) {
-   
     const range = props.range
-    const start = 0
-    const end =  start + range
+    const [start,setStart] = useState(0)
+    const [end,setEnd] = useState(start + range)
+   /*  const [intervalId, setIntervalId] = useState() */
     const items = props.data
+   /*  const interval = props.interval * 1000 */
 
+    console.log(start)
 
 
     const itemView = (item) => (
-        <div className='card item '>
-            <img className='ImageCity' alt='' src={item.url} />
+        <div className='card item ' key={item.name}>
+            <img className='ImageCity' alt='/* event.name */' src={item.url} />
             <p className='move'></p>
-            <p className='title'>{item.title}</p>
-
-                
+            <p className='title'>{item.title}</p>           
             
         </div>
     )
 
+
+/*     useEffect(() =>{
+        let id = setInterval(function (){
+            next()
+        }, interval)
+
+
+        setIntervalId(id)
+
+        return() => clearInterval(intervalId);
+
+        }, [start])
+     */
+
+    function previous( ) {
+        if(start >= range){
+    setStart( start-range)
+    setEnd (end-range)
+        }
+}
+
+function next (){
+    if(end < items.length){
+    setStart(start+range)
+    setEnd( end+range )
+    }
+}
+
+
+
+console.log("start: " + start)
+console.log("end: " + end)
+
+
+
+
+
     return (
     <div className='carouselButoom'>
-        <button className='buttom' type="" placeholder='<' ></button>
+        
         <div className='slide CarouselStyle'>
-            
+            <Arrow icon={'<'} click={previous} /> 
             {items.slice(start, end).map(itemView)}
+            <Arrow icon= {'>'} click={next} />
         </div>
-        <button className='buttom' type=""></button>
+        
     </div>
     )
 }
