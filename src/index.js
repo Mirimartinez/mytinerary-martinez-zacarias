8a2b6df8284229624447389b1974bf147f154516
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-24 00:24:28 -0300

    queda solo App.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import Footer from './component/Footer.js';
-
 import './index.css';
-/* import App from './pages/App.js'; */
-import Hero from './component/Hero';
-/* import UnderConstruction from './pages/UnderConstruction.js';
-import FooterLayout from './layouts/FooterLayout.js'; */
+import App from './pages/App.js';
 import reportWebVitals from './reportWebVitals';
-import EventsCarousel from './component/EventsCarousel.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <UnderConstruction />
-    <FooterLayout /> */}
-    {/* <App /> */}
-    <Hero />
-    <EventsCarousel />
-    <Footer />
+    <App /> 
   </React.StrictMode>
 );
 
