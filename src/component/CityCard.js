e66bfc466b8703e46e37d90553425cda2a92b342
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-28 12:43:01 -0300

    borra p vacio

--- src/component/CityCard.js
+++ src/component/CityCard.js
@@ -4,7 +4,6 @@ export default function CityCard(props) {
         <div className='CityCard'>
         <img className='CityCard-Image' alt={city.title} src={city.url} />
         <h3 className='CityCard-title'>{city.title}</h3>           
-        <h4 className='City-Card-subtitle'></h4>
         
     </div>
     )
