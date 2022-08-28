45f9655a355117b5da5938f3e8b5f526fb318517
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-28 17:44:25 -0300

    elimina video de fondo

--- src/component/CityCard.js
+++ src/component/CityCard.js
@@ -4,6 +4,8 @@ export default function CityCard(props) {
         <div className='CityCard'>
         <img className='CityCard-Image' alt={city.title} src={city.url} />
         <h3 className='CityCard-title'>{city.title}</h3>           
+        <h3 className='CityCard-subtitle'>{city.subtitle}</h3>           
+
         
     </div>
     )
--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -4,11 +4,6 @@ import CitiesCard from '../component/CitiesCards'
 function Cities(){
     return (
         <div className='Cities'>
-            <video id='videoCities' autoPlay loop muted>
-                <source
-                src="http://localhost:3000/video.mp4"
-                type="video/mp4" />
-            </video>
             <h1 className='Cities-title'>Cities</h1>
             
             <CitiesCard className='Cities-list'/>
