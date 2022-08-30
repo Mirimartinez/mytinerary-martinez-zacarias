67b4d8d05a233f00f51b6d110e6509bff952dac4
Miriana Martinez <mirianamartinezok@gmail.com>
2022-08-30 15:04:23 -0300

    modifica espacios en blanco

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -5,7 +5,6 @@ function Cities(){
     return (
         <div className='Cities'>
             <h1 className='Cities-title'>Cities</h1>
-            
             <CitiesCard className='Cities-list'/>
         </div>
     )
