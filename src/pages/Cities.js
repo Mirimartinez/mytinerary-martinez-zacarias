169ff1ec97659515b5f2f0dceb37cb752e56adaf
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-30 12:33:47 -0300

    modificaciones del cities.js

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -9,6 +9,7 @@ function Cities(){
                 src="http://localhost:3000/video.mp4"
                 type="video/mp4" />
             </video>
+            
             <h1 className='Cities-title'>Cities</h1>
             
             <CitiesCard className='Cities-list'/>
