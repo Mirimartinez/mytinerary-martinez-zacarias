a95c075af8b1823f74bc4dc94295b02c8b4f41f2
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-09-01 05:09:39 -0300

    se cambio paarte del archivo

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,22 +1,46 @@
 import '../styles/Cities.css'
-import CitiesCard from '../component/CitiesCards'
+import axios from 'axios'
+import { useEffect,useState } from 'react'
+import CityCard from '../component/CityCard'
 
 function Cities(){
+
+    const [cities, setCities] = useState([])
+
+    useEffect(() =>{
+        
+        axios.get('http://localhost:4000/cities/')
+        
+        .then(res => setCities(res.data.response))
+        console.log(cities)
+    },[])
+
+
     return (
+
+            
+
         <div className='Cities'>
 
-            <video id='videoCities' autoPlay loop muted>
+            {/* <video id='videoCities' autoPlay loop muted>
                 <source
-                src="http://localhost:3000/video.mp4"
+                src="http://localhost:3000/videoHero.mp4"
                 type="video/mp4" />
-            </video>
+            </video> */}
             
 
             <h1 className='Cities-title'>Cities</h1>
             
-            <CitiesCard className='Cities-list'/>
+            <div className='Cities-container'>
+            {cities.cities.map(CityCard)}
+            
+        </div>
         </div>
     )
 }
 
 export default Cities
+
+
+
+
