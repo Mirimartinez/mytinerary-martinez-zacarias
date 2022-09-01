2a658ff03b69a3c09b08c223722c271685b1fa18
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-01 06:45:30 -0300

    modifica ruta Detalles

--- src/component/CityCard.js
+++ src/component/CityCard.js
@@ -3,23 +3,19 @@ import '../styles/CityCard.css'
 
 function CityCard(props) {
     let city = props.city
- const country= props.country
-
-     const photo =   props.photo
-       const population = props.population
-        const fundation = props.fundation
+    const country= props.country
+    const photo =   props.photo
+    const population = props.population
+    const fundation = props.fundation
 
 
     return(
         <div className='CityCard'>
-        
-  
             <h1 className=''>{city}</h1>
             <img className='CityCard-Image' alt={city} src={photo} />
             <h3 className=''>{country}</h3>
             <p className=''>The current population of {city} in 2022 is{population}.</p>
-            <p>It was founded in {fundation}</p>
-                    
+            <p>It was founded in {fundation}</p>    
         </div>
     )
 }
