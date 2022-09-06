35868854399250385295a5931a738f07a20f4a2d
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-09-06 20:50:49 -0300

    se agrego renderizado por redux

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,27 +1,31 @@
 import '../styles/Cities.css'
-import axios from 'axios'
-import { useEffect,useState } from 'react'
+/* import axios from 'axios' */
+/* import { useEffect,useState } from 'react' */
 import CityCard from '../component/CityCard'
+
+import {useGetAllCitiesQuery} from'../features/citiesApi'
 
 function Cities(){
 
-    const [cities, setCities] = useState([])
+    
 
-    useEffect(() =>{
+    const{
+        data : cities,
+        error,
+        isLoading,
+        isSuccess,
+        isFailed
+
         
-        axios.get('http://localhost:4000/cities/')
-        
-        .then(res => setCities(res.data.response))
-        console.log(cities)
-    },[])
-
-
+    } = useGetAllCitiesQuery()
+    
+    console.log(cities)
     return (
-
+        
         <div className='Cities'>
             <h1 className='Cities-title'>Cities</h1>
             <div className='Cities-container'>
-            {cities.cities?.map(CityCard)}
+            {cities?.response.cities.map(CityCard)}
             </div>
         </div>
     )
