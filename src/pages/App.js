af15e67b58878bdf7898a2b46652ba7540596d4b
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-07 06:52:09 -0300

    comenta para probar editCity

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,6 +5,7 @@ import Layout from '../layouts/Layout'
 import NewCity from '../pages/NewCity'
 import Cities from '../pages/Cities'
 import CityDetails from '../component/CityDetails'
+import EditCity from './EditCity'
 import UnderConstruction from './UnderConstruction'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ScrollToTop from '../component/ScrollToTop'
@@ -15,8 +16,8 @@ export default function App() {
     <BrowserRouter>
     <ScrollToTop/>
       <Layout>
-    <Routes>
-
+    <EditCity/>
+    {/* <Routes>
 
     <Route  path='/' element={<Home/>} />
     <Route path='/Cities' element={<Cities/>} /> 
@@ -24,7 +25,7 @@ export default function App() {
     <Route path='/CityDetails' element={<CityDetails/>}/>
     <Route path='*' element={<UnderConstruction />} />
     
-    </Routes>
+    </Routes> */}
     </Layout>
     </BrowserRouter>
   )
