80106dc215718880f9ea3b83f668350c3f2145c5
Teje8 <jteruel8@gmail.com>
2022-09-07 14:55:11 -0300

    add edit city link

--- src/pages/App.js
+++ src/pages/App.js
@@ -7,6 +7,7 @@ import CityDetails from '../component/CityDetails'
 import UnderConstruction from './UnderConstruction'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ScrollToTop from '../component/ScrollToTop'
+import EditCity from './EditCity'
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
     <Route  path='/' element={<Home/>} />
     <Route path='/Cities' element={<Cities/>} /> 
     <Route path='/NewCity' element={<NewCity/>} />
+    <Route path='/EditCity' element={<EditCity/>} />
 
     <Route path='/CityDetails' element={<CityDetails/>}/>
     <Route path='' element={<UnderConstruction />} />
