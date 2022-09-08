e77ce8ef5af9d2004de45535845d0a8a5aaa4053
Teje8 <jteruel8@gmail.com>
2022-09-08 04:08:56 -0300

    aply redux

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -2,31 +2,31 @@ import '../styles/Cities.css'
 /* import axios from 'axios' */
 /* import { useEffect,useState } from 'react' */
 import CityCard from '../component/CityCard'
+import InputSearch from '../component/InputSearch'
 
 import {useGetAllCitiesQuery} from'../features/citiesApi'
 
 function Cities(){
 
-    
-
-    const{
+    let{
         data : cities,
         error,
         isLoading,
         isSuccess,
         isFailed
-
-        
     } = useGetAllCitiesQuery()
-    
+    let content 
+    if(isLoading){
+        cities = []
+    }
     console.log(cities)
     return (
         
         <div className='Cities'>
             <h1 className='Cities-title'>Cities</h1>
-
+            <InputSearch />
             <div className='Cities-container'>
-            {cities?.response.cities.map(CityCard)}
+            {cities?.map(CityCard)}
             </div>
 
         </div>
@@ -34,7 +34,3 @@ function Cities(){
 }
 
 export default Cities
-
-
-
-
