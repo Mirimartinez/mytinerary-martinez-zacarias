68b2dcc03a40fcb9fd22c1afa2ef979de69d9396
Teje8 <jteruel8@gmail.com>
2022-09-08 17:31:33 -0300

    select & edit form

--- src/pages/EditCity.js
+++ src/pages/EditCity.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from '../component/Input'
 import '../styles/EditCity.css'
 import axios  from 'axios'
 import { useEffect ,useState } from 'react'
-import Option from '../component/OptionSelect'
 
 function EditCity() {
-    const [cities, setCities] = useState({})
+    const [cities, setCities] = useState([])
+    const selector = useRef()
+
     useEffect(() => {
-        axios.get("http://localhost:4000/cities/")
+        axios.get(`http://localhost:4000/cities/`)
         .then(response => setCities(response.data))
     }, [])
-        console.log(cities);
+
+    const [city, setCity] = useState({})
+
+    const captureData = (e) =>{
+        const{name, value} = e.target
+        setCity({...city, [name]:value})
+    }
+    
+    const saveData = async(e)=>{
+        e.preventDefault()
+        await axios.patch(`http://localhost:4000/cities/${selector.current.value}`, city)
+    }
+
+
+
         const datos = [
         {key: 'City', for: 'city', type: 'text'},
         {key: 'Country', for: 'country', type: 'text'},
         {key: 'Population', for: 'population', type: 'number'},
         {key: 'Photo', for: 'photo', type: 'text'},
         {key: 'Foundation', for: 'foundation', type: 'date'},
-    ]
-
-    // const saveData = async(e)=>{
-    //     e.preventDefault()
-    //         await axios.put('http://localhost:4000/cities/', editCity)
-    //         setCities({...initialValor})
-    //     }
+        ]
 
     return (
         <div className='InputSelectImgContainer'>
@@ -33,15 +42,15 @@ function EditCity() {
                 <source src="http://localhost:3000/videoHero.mp4" type="video/mp4" />
             </video>
             <div className='InputSelectContainer'>
-                <select className='EditSelect'>
-                    {cities.response?.map(city =>
-                <Option city = {city} />
+                <select ref={selector} className='EditSelect'>
+                    {cities.map(city =>
+                    <option className='OptionSelect' key={city._id} value={city._id}>{city.city}</option>
                     )}
                 </select>
                 </div>
-                <form >
-                    {datos.map(dato => <Input label={dato.key} for={dato.for} type={dato.type}/>)}
-                    <button type='submit' className='ButtonInput'>Submit</button>
+                <form onSubmit={saveData}>
+                    {datos.map(dato => <Input key={dato.key} four={dato.for} type={dato.type} change={captureData}/>)}
+                    <button type='submit' className='ButtonInput' >Submit</button>
                 </form>
                 <div className='EditCityImgContainer'>
                     <img className='EditCityImg' src="/svg/man-at-work.png" alt="" /> 
