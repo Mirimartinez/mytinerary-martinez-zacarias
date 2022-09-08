9c344de98ef6d8ff9028a5ab0895d51ff8f72d5a
Teje8 <jteruel8@gmail.com>
2022-09-08 04:02:44 -0300

    add id to card

--- src/component/CityCard.js
+++ src/component/CityCard.js
@@ -2,20 +2,20 @@ import '../styles/CityCard.css'
 import {Link as LinkRouter} from 'react-router-dom'
 
 function CityCard(props) {
+    // const cities  = props.data
     let city = props.city
     let country= props.country
-    const photo = props.photo
+    let photo = props.photo
+    let id = props._id
     return(
-        <LinkRouter to='/CityDetails'>
-        <div className='CityCard'>
-
-        <img className='CityCard-Image' alt={city} src={photo} />
-        <h3 className='CityCard-title'>{city}</h3>           
-        <h3 className='CityCard-subtitle'>{country}</h3> 
-        </div>
-        </LinkRouter>
-
-    )
+            <LinkRouter to={`/cities/${id}`}>
+                <div className='CityCard'>
+                    <img className='CityCard-Image' alt="" src={photo} />
+                    <h3 className='CityCard-title'>{city}</h3>           
+                    <h3 className='CityCard-subtitle'>{country}</h3> 
+                </div>
+            </LinkRouter>
+        )
 }
 
 
