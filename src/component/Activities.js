57e4f410914aa62dd68a88f105f99146162bbfb3
zacarias-gabriel <110680218+zacarias-gabriel@users.noreply.github.com>
2022-09-08 03:42:57 -0300

    se modifico activities

--- src/component/Activities.js
+++ src/component/Activities.js
@@ -3,17 +3,25 @@ import '../styles/Activities.css'
 function Activity(props){
     const activities = [
         {
-            name: 'Best activity EVER!',
-            photo: '',
+            name: 'Outdoor yoga',
+            photo: 'https://northernvirginiamag.com/wp-content/uploads/2021/04/outdooryoga.jpg',
+        },
+        {
+            name: 'Guided tour!',
+            photo: 'https://i0.wp.com/www.disfrutarosario.com/wp-content/uploads/2019/04/Circuito-del-Puerto-1.jpg?resize=1024%2C682&ssl=1',
+        },
+        {
+            name: 'Visit gourmet restaurants',
+            photo: 'https://blog.miranterestaurante.com.br/wp-content/uploads/2018/09/chef-1024x550.jpg',
         }
     ]
 
     const activityCard = (item) =>{
 
         return(
-        <div className="Activities-card">
+        <div className="Activities-card" style={{ backgroundImage: `url(${item.photo})`}}>
             <p className='Activities-text'>{item.name}</p>
-            <p>{item.photo}</p>
+            
         </div>)
     }
 
