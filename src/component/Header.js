3672638cf716852fae39d91a7e07f9ae8d3baee2
Teje8 <jteruel8@gmail.com>
2022-09-08 04:04:35 -0300

    fix header

--- src/component/Header.js
+++ src/component/Header.js
@@ -31,15 +31,11 @@ function Header() {
     
     return (
         <div className='Header-cont'>
-            <div id="Header-logo">
-            <img className="Header-logo" src="http://localhost:3000/logo.png" alt="logo"/>
+            <div className='PagesLinks'>
+                {pages.map(link)}
             </div>
-            <div id="Header-title" className="Header-color">
-            <h1 className="Header-title">MyTinerary</h1>
-            </div>
-            <div> 
-                <img className='Header-profile' alt='profile' src="https://media0.giphy.com/media/kcZlnhiaB1p76tKS6S/giphy.gif?cid=790b7611658a2c1837a6b243c6e5b7c5f9203dcba641b933&rid=giphy.gif&ct=s" />
-                <nav className='Header-nav' > 
+            <div  className='Header-nav' > 
+                <nav> 
                 {
                     open
                     ? <ul>
@@ -48,10 +44,18 @@ function Header() {
                         : null
                     }
                 </nav>
-            {pages.map(link)}
             <button className="Header-nav Header-a" onClick={handleOpenMenu}>Menu</button>
         </div>
-        </div>
+            <div id="Header-logo">
+            <img className="Header-logo" src="http://localhost:3000/logo.png" alt="logo"/>
+            <h1 className="Header-title">MyTinerary</h1>
+            {/* </div>
+            <div id="Header-title" className="Header-color"> */}
+            </div>
+                <div className='Header-profile' >
+                    <img className='ImgProfile' alt='profile' src="https://media0.giphy.com/media/kcZlnhiaB1p76tKS6S/giphy.gif?cid=790b7611658a2c1837a6b243c6e5b7c5f9203dcba641b933&rid=giphy.gif&ct=s" />
+                </div>
+                </div>
     )
     }
 
