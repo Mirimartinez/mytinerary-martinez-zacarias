b4f82c244b555e350d7ca917d53c1fa2ba43fb3a
Teje8 <jteruel8@gmail.com>
2022-09-08 17:30:25 -0300

    add function

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,18 +1,15 @@
 import '../styles/Input.css' 
 
 function Input(props) {
-    let inputFor = props.for
+    let inputFor = props.four
     let type = props.type
     let label = props.label
-
-
-
+    
     return(
         <div className='Input-form'>
-                <label className='Input-label' for={inputFor}>{label}</label>
-                <input className='Input-input' type={type} id={inputFor} name={inputFor}></input>
+                <label className='Input-label' htmlFor={inputFor}>{label}</label>
+                <input className='Input-input' onChange={props.change} type={type} id={inputFor} name={inputFor}></input>
         </div>
     )
-        
 }
 export default Input
