23fcac878f97b7158235e054d41bf875d5712618
Teje8 <jteruel8@gmail.com>
2022-09-10 22:54:00 -0300

    itinerary creator photo

--- src/component/Itinerary.js
+++ src/component/Itinerary.js
@@ -14,7 +14,7 @@ function Itinerary(props){
             likes:'18',
             tags:'#',
             duration:'2 weeks',
-            photo: 'https://images.pexels.com/photos/11013903/pexels-photo-11013903.jpeg'
+            photo: 'https://media.istockphoto.com/photos/mature-ethnic-man-wearing-eyeglasses-at-home-picture-id1319763230?b=1&k=20&m=1319763230&s=170667a&w=0&h=vqnbgy5ln6NvFK6OqRxl1jtIXstEN3xkWjVHMAbTKH8='
         }
     ]
 
@@ -34,17 +34,19 @@ function Itinerary(props){
     const itineraryCard = (item) =>{
 
         return(
-            <div className='ItineraryCard ItineraryCard-subtitle' /* style={{ backgroundImage: `url(${item.photo})`}} */>
-            <div className='Itinerary-user'>    
-                <p>Name: {item.name}</p>
-                <p>User: {item.user}</p>
-            </div>
-
-            <div className='Itinerary-itinerary'>
-                <p>Price: {item.price}</p>
-                <p>LIkes: ♥{item.likes}</p>
-                <p>Tags: {item.tags}</p>
-                <p>Duration: {item.duration}</p>
+            <div className='ItineraryCard ItineraryCard-subtitle'>
+            <div className='CreatorItinerary'>
+                <div className='Itinerary-user'>
+                    <img src={item.photo} alt='img' className='CreatorImg'></img>
+                    <p>Name: {item.name}</p>
+                    <p>User: {item.user}</p>
+                </div>
+                <div className='Itinerary-itinerary'>
+                    <p>Price: {item.price}</p>
+                    <p>LIkes: ♥{item.likes}</p>
+                    <p>Tags: {item.tags}</p>
+                    <p>Duration: {item.duration}</p>
+                </div>
             </div>
             <div>
             <div className='Itinerary-activities'>
