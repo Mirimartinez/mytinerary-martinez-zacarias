6cc8b9d75c9dc9df3f930e02d16886a6e1e0b22b
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-15 15:57:19 -0300

    in process

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import SignUpGoogle from '../component/SignUpGoogle'
+import SignUpComp from '../component/SignUpComp'
 
 export default function SignUp() {
     return (
         <div>
+            <SignUpComp/>
             <SignUpGoogle/>
         </div>
     )
