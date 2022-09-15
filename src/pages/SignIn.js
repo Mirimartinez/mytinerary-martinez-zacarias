453de9b940b000b91aed4e458916677cc6f57a4b
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-15 16:00:06 -0300

    add SignIn page in process

---
+++ src/pages/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import SignInGoogle from '../component/SignInGoogle'
+//import SignInComp from '../component/SignUpComp'
+
+function SignIn() {
+    return (
+        <div>
+            {/* <SignInComp/> */}
+            {/*<SignInGoogle/>*/}
+        </div>
+    )
+}
+
+export default SignIn
