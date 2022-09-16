003c4d736cd769809a2bcf419b1bcb3ddf10e69c
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-16 07:57:36 -0300

    delete duplicated file

--- src/component/Input.js
+++ src/component/Input.js
@@ -4,11 +4,14 @@ function Input(props) {
     let inputFor = props.four
     let type = props.type
     let label = props.label
+    let key = props.key
+    let Value = props.value
+
     
     return(
         <div className='Input-form'>
                 <label className='Input-label' htmlFor={inputFor}>{label}</label>
-                <input className='Input-input' onChange={props.change} type={type} id={inputFor} name={inputFor} placeholder={props.value}></input>
+                <input className='Input-input' ref={props.valueRef} onChange={props.change} type={type} id={inputFor} name={inputFor} placeholder={props.value}></input>
         </div>
     )
 }
