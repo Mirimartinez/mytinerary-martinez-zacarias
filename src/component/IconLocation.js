91c5212ff65532cb3b8f48dc59c4d9d5b6883336
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-23 12:51:34 -0300

    add iconLocation component

---
+++ src/component/IconLocation.js
@@ -0,0 +1,11 @@
+import L from 'leaflet'
+
+export const IconLocation = L.icon({
+    iconUrl: 'https://images.vectorhq.com/images/previews/c42/map-pin-126760.png',
+    iconRetinaUrl: 'https://images.vectorhq.com/images/previews/c42/map-pin-126760.png', iconAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [35, 35],
+    className: "leaflet-venue-icon",
+})
