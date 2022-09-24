fecef62201e5067a2065a04d052a32070ebc74fc
Teje8 <jteruel8@gmail.com>
2022-09-23 23:55:15 -0300

    add user logic

--- src/pages/App.js
+++ src/pages/App.js
@@ -11,23 +11,35 @@ import ScrollToTop from '../component/ScrollToTop'
 import MyTineraries from './MyTineraries'
 import SignUpPage from './SignUpPage'
 import SignInPage from './SignInPage'
+import NewItinerary from '../pages/NewItinerary'
+import PatchItinerary from '../component/PatchItinerary'
 
 
 
 export default function App() {
+
+  let user = "";
+
+  if (localStorage.getItem("user")) {
+    user = JSON.parse(localStorage.getItem("user")).role;
+  } else {
+  }
+
   return (
     <BrowserRouter>
       <ScrollToTop/>
       <Layout>
         <Routes>
           <Route  path='/' element={<Home/>} />
-          <Route  path='/auth/signup' element={<SignUpPage/>} />
-          <Route path='/auth/signin' element={<SignInPage/>}/>
+          <Route  path='/auth/signup' element={user === "" ? <SignUpPage/> : <UnderConstruction /> }/>
+          <Route path='/auth/signin' element={user === "" ? <SignInPage/> : <UnderConstruction /> }/>
           <Route path='/Cities' element={<Cities/>} /> 
-          <Route path='/NewCity' element={<NewCity/>} />
-          <Route path='/EditCity' element={<EditCity/>} />
+          <Route path='/NewCity' element={user === "admin" ? <NewCity/> : <UnderConstruction /> } />
+          <Route path='/EditCity' element={user === "admin" ? <EditCity/> : <UnderConstruction /> } />
           <Route path='/cities/:id' element={<City/>}/>
-          <Route path='/MyTineraries' element={<MyTineraries/>}/>
+          <Route path='/Mytineraries/patchitinerary' element={user === "" ? <UnderConstruction /> : <PatchItinerary/>}/>
+          <Route path='/MyTineraries' element={user === "" ? <UnderConstruction />  : <MyTineraries/> }/>
+          <Route path='/NewItinerary' element={user === "" ? <UnderConstruction />  : <NewItinerary/> }/>
           <Route path='*' element={<UnderConstruction />} />
         </Routes> 
       </Layout>
